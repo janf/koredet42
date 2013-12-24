139f32b82af55f83a5927b3a7113e38a9fd2743c
Jan Fredrik <janfredrik@havie.net>
2013-12-24 13:02:43 +0100

    Working version  with user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Myhome::Application.routes.draw do
+  devise_for :users
+  resources :dashboard
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
