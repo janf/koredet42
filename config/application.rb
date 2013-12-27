3ae521b9af600cc9c7b2dc30abd3592f3929c60c
Jan Fredrik <janfredrik@havie.net>
2013-12-27 08:17:23 +0100

    Stylesheets added, views updated

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Myhome
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.initialize_on_precompile = false
     config.action_controller.include_all_helpers = false
   end
